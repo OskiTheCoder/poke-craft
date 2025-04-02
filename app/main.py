59db8f307c4bf8b86ac0a55e913abf8949ddadc3
shelvy <sks91@berkeley.edu>
2025-04-01 19:16:06 -0700

    flush out more project structure

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from app.api.v1 import pokemon, users
 
-app = FastAPI(title="TBD")
+app = FastAPI(title="PokeCraft API", version="1.0")
+
+app.include_router(pokemon.router, prefix="/pokemon", tags=["Pokemon"])
+app.include_router(users.router, prefix="/users", tags=["Users"])
 
 @app.get("/")
 def root():
-    return {"message": "Hello, World!"}
+    return {"message": "Welcome to the PokeCraft API!"}
