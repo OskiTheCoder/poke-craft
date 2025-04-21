f14789e9becc5699d8c3157f38baf3bdcea9c565
shelvy <sks91@berkeley.edu>
2025-04-20 18:57:16 -0700

    docker

--- app/data/fake_data.py
+++ app/data/fake_data.py
@@ -1,5 +1,14 @@
 FAKE_USERS_POKEMON = {
     "user_1": ["Pikachu", "Charmander"],
     "user_2": ["Bulbasaur"],
+    "user_3": ["Squirtle", "Jigglypuff", "Meowth"],
+    "user_4": ["Eevee", "Snorlax"],
+    "user_5": ["Psyduck", "Magikarp", "Gengar"],
 }
-FAKE_POKEMON_LIST = ["Pikachu", "Charmander", "Squirtle", "Bulbasaur", "Eevee"]
+
+FAKE_POKEMON_LIST = [
+    "Pikachu", "Charmander", "Squirtle", "Bulbasaur", "Eevee",
+    "Snorlax", "Jigglypuff", "Meowth", "Psyduck", "Magikarp",
+    "Gengar", "Vulpix", "Growlithe", "Dratini", "Lapras",
+    "Cubone", "Onix", "Machop", "Abra", "Rattata"
+]
